357a29391dbeba3b0c6724d2d1646019b233e106
Andrew Kane <andrew@getformidable.com>
2011-06-17 00:46:49 -0700

    Updated permissions

--- cities.rb
+++ cities.rb
@@ -11,7 +11,7 @@ require "amqp"
 EventMachine.run do
   AMQP.start do |connection|
     channel  = AMQP::Channel.new(connection)
-    exchange = channel.topic("pub/sub", :auto_delete => true)
+    exchange = channel.topic("cities", :auto_delete => true)
 
     # Subscribers.
     channel.queue("", :exclusive => true) do |queue|
